b0ceb263936d364e5be036411d89b48a539b428c
David Golden <dagolden@cpan.org>
2010-03-12 00:02:21 +0000

    Import of DAGOLDEN/Metabase-Fact-0.003002 from CPAN.
    gitpan-cpan-distribution: Metabase-Fact
    gitpan-cpan-version:      0.003002
    gitpan-cpan-path:         DAGOLDEN/Metabase-Fact-0.003002.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Metabase/Resource.pm
+++ lib/Metabase/Resource.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Carp ();
 
-our $VERSION = '0.003001';
+our $VERSION = '0.003002';
 $VERSION = eval $VERSION;
 
 #--------------------------------------------------------------------------#
@@ -165,7 +165,7 @@ Takes a single resource string argument and constructs a new Resource object
 from a resource subtype determined by the URI scheme.  Throws an error if the
 required resource subclass is not available.
 
-=head2 content
+=head2 resource
 
 Returns the string used to initialize the resource object.
 
